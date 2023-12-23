b93bd9034a89ea782f6e3e6a6fcd66446fe431a5
MohamedJamh <jam7.mohamed@gmail.com>
2023-12-23 15:44:29 +0100

    set up

---
+++ src/main/java/com/taskflow/web/rest/HomeController.java
@@ -0,0 +1,16 @@
+package com.taskflow.web.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping("")
+    public String home() {
+        return "index";
+    }
+}
