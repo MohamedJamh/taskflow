5195101089523569144135c7d8e7af96c6a4e8b9
MohamedJamh <jam7.mohamed@gmail.com>
2024-01-02 11:41:21 +0100

    v1 : add - app seeder is enable based on .env

--- src/main/java/com/taskflow/_db/seeder/AppSeeder.java
+++ src/main/java/com/taskflow/_db/seeder/AppSeeder.java
@@ -6,16 +6,23 @@ import com.taskflow._db.seeder.seeds.RoleSeeder;
 import com.taskflow._db.seeder.seeds.UserSeeder;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class AppSeeder {
+    @Value("${seeder.enabled}")
+    private Boolean seederEnabled;
+
     private final PermissionSeeder permissionSeeder;
     private final RoleSeeder roleSeeder;
     private final UserSeeder userSeeder;
+
     @PostConstruct
     public void init() {
+        if(Boolean.FALSE.equals(seederEnabled)) return;
+        System.out.println("------------------------------------Seeding database------------------------------------");
         permissionSeeder.seed();
         roleSeeder.seed();
         userSeeder.seed();
